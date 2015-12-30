b2e6a36a4317a2119cf0f738f8edfc8fe62b6b8c
Ben Clist <ben@benclist.com>
2015-12-30 16:58:56 +1300

    Basic Form

--- index.php
+++ index.php
@@ -6,7 +6,22 @@
 
 <body>
     <h1>Simple GST Calculator</h1>
-    <h2>Coming Soon!</h2>
+    <form>
+        Amount excl. GST:
+        <br>
+        <input type="text" name="exclgst">
+        <br>
+        <br> Amount incl. GST:
+        <br>
+        <input type="text" name="inclgst">
+        <br>
+        <br>
+        <input type="submit" value="Calculate">
+        <br>
+        <br> GST Value:
+        <br>
+        <input type="text" name="gst">
+    </form>
 </body>
 
 </html>
